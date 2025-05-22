3cbafb307a6c3c51efb383fd6b577d6fcf06c365
Kedar Jevargi <kedarjevargi@Kedars-MacBook-Pro.local>
2025-05-22 22:49:50 +0530

    v2

--- main.py
+++ main.py
@@ -2,7 +2,29 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+students = {
+    101: "Alice",
+    102: "Bob",
+    103: "Charlie",
+    104: "David",
+    105: "Eva",
+    106: "Frank",
+    107: "Grace",
+    108: "Hannah",
+    109: "Ivan",
+    110: "Julia"
+}
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+
+@app.get("/students/{id}")
+
+async def fetch(id:int):
+    if id in students:
+        return{"Name":f"{students[id]}"}
+    else:
+        return "Not found"
