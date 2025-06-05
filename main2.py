2f285b7c741d9773303d5a7a2fdb0a30169c93fd
Kedar Jevargi <kedarjevargi@Kedars-MacBook-Pro.local>
2025-06-06 00:13:20 +0530

    CRUD operations done

--- main2.py
+++ main2.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Path, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware  # Add this import
 from pydantic import BaseModel, Field, field_validator, computed_field
 import json
-
+from typing import Optional
 app = FastAPI()
 
 # Add CORS middleware - ADD THIS SECTION
@@ -62,6 +62,64 @@ class Patient(BaseModel):
             return "Obesity Class II (Severe)"
         else:
             return "Obesity Class III (Very Severe or Morbid)"
+        
+
+class Patient_update(BaseModel):
+    name: Optional[str] = Field(default=None, max_length=20)
+    city: Optional[str] = Field(default=None,max_length=20)
+    age: Optional[int] = Field(default=None,gt=1, le=121)
+    gender: Optional[str]=Field(default=None)
+    height: Optional[float] = Field(default=None,gt=0)
+    weight: Optional[float] = Field(default=None,gt=0)
+
+
+    @field_validator("name")
+    @classmethod
+    def name_capital(cls, value):
+        return value.capitalize()
+    
+    @field_validator("city")
+    @classmethod
+    def city_capital(cls, value):
+        return value.capitalize()
+    
+    @field_validator("gender")
+    @classmethod
+    def check_gender(cls, value):
+        value = value.capitalize()
+        gen_data = {"Male", "Female"}
+        if value in gen_data:
+            return value
+        raise HTTPException(status_code=400, detail=f"Invalid input. Expected one of: {gen_data}")
+    
+    @computed_field
+    @property
+    def bmi(self) -> Optional[float]:
+        if self.weight is not None and self.height is not None:
+            return round(self.weight / (self.height ** 2), 2)
+        return None
+
+    @computed_field
+    @property
+    def verdict(self) -> Optional[str]:
+        bmi = self.bmi
+        if bmi is None:
+            return None
+        if bmi < 18.5:
+            return "Underweight"
+        elif bmi < 25:
+            return "Normal weight"
+        elif bmi < 30:
+            return "Overweight"
+        elif bmi < 35:
+            return "Obesity Class I (Moderate)"
+        elif bmi < 40:
+            return "Obesity Class II (Severe)"
+        else:
+            return "Obesity Class III (Very Severe or Morbid)"
+
+        
+
 
 # ---------------------- File Handling ---------------------- #
 def read_data():
@@ -133,3 +191,28 @@ def add_patient(patient: Patient):
     data[new_id] = patient.model_dump()
     write_data(data)
     return {"message": f"Patient added with ID {new_id}"}
+
+
+
+@app.put("/update/{p_id}")
+def update_patient(p_id:str,patient:Patient_update):
+    data=read_data()
+    if p_id not in data:
+        raise HTTPException(status_code=404, detail=f"Patient with ID {p_id} not found")
+    temp_data=patient.model_dump()
+    for i in temp_data:
+        if temp_data[i]!=None:
+            data[p_id][i]=temp_data[i]
+
+    write_data(data) 
+    return{"message":f"Patient with ID {p_id} updated"}       
+    
+    
+@app.delete("/delete/{p_id}")
+def del_patient(p_id:str):
+    data=read_data()
+    if p_id not in data:
+        raise HTTPException(status_code=404, detail=f"Patient with ID {p_id} not found")
+    data.pop(p_id)
+    write_data(data)
+    return{"message":f"Patient with ID {p_id} deleted"}       
